d8b3c01ad0033bb3075ca3ecfe510fd8444d2255
Robert Sliwinski <r@sliwi.org>
2023-10-02 21:26:06 +0200

    feat: The very first initial commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "robsliwi's blog as a Nix flake";
+
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # to work with older version of flakes
+        lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+
+        # Generate a user-friendly version number.
+        version = builtins.substring 0 8 lastModifiedDate;
+
+        # pkgs is easier to write then some lengthy ${system} thing
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        # no default package (yet)
+        # packages.default = pkgs.hello;
+        devShells.default = with pkgs; mkShell {
+          packages = [ hugo ];
+        };
+      }
+    );
+}
